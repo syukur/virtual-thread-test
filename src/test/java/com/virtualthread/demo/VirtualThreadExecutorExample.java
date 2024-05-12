60468418eb23a926d6b0203096e21934161850fb
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2024-05-12 17:31:26 +0700

    virtual thread with executor

---
+++ src/test/java/com/virtualthread/demo/VirtualThreadExecutorExample.java
@@ -0,0 +1,29 @@
+/**
+ * Java program for Creating and executing
+ * Virtual Thread with the
+ * Executor.newVirtualThreadPerTaskExecutor
+* */
+
+package com.virtualthread.demo;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class VirtualThreadExecutorExample {
+    public static void main(String[] args) {
+        try (ExecutorService myExecutorService = Executors.newVirtualThreadPerTaskExecutor()){
+            //Submit a task that print a message
+            Future<?> future = myExecutorService.submit(() ->{
+                System.out.println("Running thread");
+            } );
+
+            //Wait for the tas to complete
+            future.get();
+            System.out.println("Program Complete !!");
+        }catch (InterruptedException | ExecutionException e){
+            e.printStackTrace();
+        }
+    }
+}
