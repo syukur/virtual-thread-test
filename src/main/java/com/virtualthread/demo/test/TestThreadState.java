62128241f2db80199d29428e5481923f2db5e163
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2024-05-18 20:02:43 +0700

    test state of thread

---
+++ src/main/java/com/virtualthread/demo/test/TestThreadState.java
@@ -0,0 +1,26 @@
+package com.virtualthread.demo;
+
+public class TestThreadState {
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable runnable = () -> {
+            var state0 = String.format("Thread State 0 : %s", Thread.currentThread().getState());
+            System.out.println( state0 );
+        };
+
+        var thread = new Thread(runnable);
+
+        var state1 = String.format("Thread State 1 : %s", thread.getState());
+        System.out.println(state1);
+
+        thread.start();
+
+        thread.join();
+
+        var state2 = String.format("Thread State 2 : %s", thread.getState());
+        System.out.println(state2);
+
+
+
+    }
+}
