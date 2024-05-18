728420aa4dbbd472bf4e8fb41bf36bc53ca89b67
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2024-05-18 10:05:42 +0700

    apa ?

--- src/main/java/com/virtualthread/demo/test/TestPlatformThread.java
+++ src/main/java/com/virtualthread/demo/test/TestPlatformThread.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 public class TestPlatformThread {
     public static void main(String[] args) {
 
-        for ( int i=0; i<= 10; i ++ ){
+        for ( int i=0; i<= 10_000; i ++ ){
 
             Thread.ofPlatform()
                    .name("Elfathan.Thread: " + i)
