9d16b1a2587c655b977ec18734606d6dbc8743fb
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2024-05-19 12:02:05 +0700

    test synchronize

--- src/main/java/com/virtualthread/demo/test/Counter.java
+++ src/main/java/com/virtualthread/demo/test/Counter.java
@@ -1,10 +1,38 @@
 package com.virtualthread.demo.test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Counter{
+
+    DateTimeFormatter dateTimeFormatter =  DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss SSSSSSSSS");
     private Long value = 0L;
 
+//    public synchronized void increment(){
+//        value++;
+//    }
+
     public void increment(){
-        value++;
+
+        var now = LocalDateTime.now();
+        var nowOnText = now.format(dateTimeFormatter);
+
+        System.out.println( nowOnText + " before lock: " + Thread.currentThread().getName() );
+
+        synchronized (this){
+
+             now = LocalDateTime.now();
+             nowOnText = now.format(dateTimeFormatter);
+
+            System.out.println( nowOnText + " on lock block: " + Thread.currentThread().getName() );
+            value++;
+        }
+
+
+        now = LocalDateTime.now();
+        nowOnText = now.format(dateTimeFormatter);
+
+        System.out.println( nowOnText + " after lock: " + Thread.currentThread().getName() );
     }
 
     public Long getValue(){
--- src/main/java/com/virtualthread/demo/test/RaceConditionTest.java
+++ src/main/java/com/virtualthread/demo/test/RaceConditionTest.java
@@ -1,26 +1,40 @@
 package com.virtualthread.demo.test;
 
+import java.time.Duration;
+
 public class RaceConditionTest {
 
     public static void main(String[] args) throws InterruptedException {
         Counter counter = new Counter();
 
         Runnable runnable = () -> {
-            for (int i = 0; i < 1000000; i++)
-                counter.increment();
+            for (int i = 0; i < 100; i++){
+                try {
+                    Thread.sleep(Duration.ofSeconds(2));
+                    counter.increment();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+
         };
 
-        Thread thread1 = Thread.ofVirtual().start(runnable);
-        Thread thread2 = Thread.ofVirtual().start(runnable);
-        Thread thread3 = Thread.ofVirtual().start(runnable);
+//        Thread thread1 = Thread.ofVirtual().start(runnable);
+//        Thread thread2 = Thread.ofVirtual().start(runnable);
+//        Thread thread3 = Thread.ofVirtual().start(runnable);
 
-//        Thread thread1 = new Thread(runnable);
-//        Thread thread2 = new Thread(runnable);
-//        Thread thread3 = new Thread(runnable);
-//
-//        thread1.start();
-//        thread2.start();
-//        thread3.start();
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+
+        thread1.setName("thread.01");
+        thread2.setName("thread.02");
+        thread3.setName("thread.03");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
         thread1.join();
         thread2.join();
