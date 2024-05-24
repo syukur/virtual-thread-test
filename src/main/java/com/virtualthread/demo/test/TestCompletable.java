d59d8f22f2983cf99f4b162e8cd98dfcae1fbaea
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2024-05-24 10:35:18 +0700

    Completable Test 01

--- src/main/java/com/virtualthread/demo/test/TestCompletable.java
+++ src/main/java/com/virtualthread/demo/test/TestCompletable.java
@@ -1,11 +1,13 @@
 package com.virtualthread.demo.test;
 
 import java.time.Duration;
+import java.util.Random;
 import java.util.concurrent.*;
 
 public class TestCompletable {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        System.out.println(getValue().get());
+        //System.out.println(getValue().get()); // test getValue
+        System.out.println(getFastest().get());
     }
 
     /** Contoh Penggunaan Completable
@@ -25,5 +27,34 @@ public class TestCompletable {
 
         return completable;
     }
+
+    private static void execute( CompletableFuture<String> completable, String value ){
+
+       var executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        Random random = new Random();
+
+        executor.execute(() -> {
+            try {
+                Thread.sleep( 1000 + random.nextInt(5000) );
+                completable.complete(value);
+            } catch (InterruptedException e) {
+                completable.completeExceptionally(e);
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static Future<String> getFastest(){
+        CompletableFuture<String> completable = new CompletableFuture<>();
+
+        execute(completable, "Thread 1");
+        execute(completable, "Thread 2");
+        execute(completable, "Thread 3");
+
+        return completable;
+    }
+
+
 
 }
