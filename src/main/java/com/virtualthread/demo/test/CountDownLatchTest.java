9c481288c41d1ebd2694ce844eafd9c4e5540c32
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2024-05-27 21:10:08 +0700

    Test CountDownLatch

---
+++ src/main/java/com/virtualthread/demo/test/CountDownLatchTest.java
@@ -0,0 +1,58 @@
+package com.virtualthread.demo.test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class CountDownLatchTest {
+
+    static DateTimeFormatter dateTimeFormatter =  DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss.SSS");
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        for (int i = 1; i <= 5; i ++){
+
+            Runnable runnable = () -> {
+                try {
+                    System.out.println( getCurrentDateTime() + "Thread: " + Thread.currentThread() + " Started");
+                    Thread.sleep(Duration.ofSeconds(2));
+                    System.out.println( getCurrentDateTime() + "Thread: " + Thread.currentThread() + " Finish");
+                    System.out.println();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }finally {
+                    countDownLatch.countDown();
+                }
+            };
+
+            executor.execute( runnable );
+        }
+
+        executor.execute(() -> {
+
+            try {
+                countDownLatch.await();
+                System.out.println(getCurrentDateTime() + "Thread: " + Thread.currentThread() + " All Task Finished");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        });
+
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+    private static String getCurrentDateTime(){
+        var now = LocalDateTime.now();
+        var nowOnText = now.format(dateTimeFormatter);
+
+        return "[" + nowOnText + "]: ";
+    }
+}
